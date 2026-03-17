8afe58f09992d646a98c39f0164a546f7070fdcd
sarchiandres <sarchiandres63@gmail.com>
2026-03-17 13:04:13 -0500

    se agrega  la seccion de sobre mi  y algunos iconos

--- src/constans/tags.js
+++ src/constans/tags.js
@@ -4,6 +4,16 @@ import MySqlIcon from "../Icons/MySql.astro";
 import TypeScriptIcon from "../Icons/TypeScriptIcon.astro";
 import Tailwind from "../Icons/Tailwind.astro";
 import AstroIcon from "../Icons/AstroIcon.astro";
+import AngularIcon from "../Icons/Angular.astro";
+import JavaIcon from "../Icons/Java.astro";
+import PostgrestIcon from "../Icons/Postgrest.astro";
+import PythonIcon from "../Icons/Python.astro";
+import PhpIcon from "../Icons/Php.astro";
+import LaravelIcon from "../Icons/LaravelIcon.astro";
+import SqlServerIcon from "../Icons/SqlServer.astro";
+import DockerIcon from "../Icons/Docker.astro";
+import GitIcon from "../Icons/GitHubIcon.astro";
+import CSharpIcon from "../Icons/CIcon.astro";
 
 export const TAGS = {
   REACT: {
@@ -29,5 +39,45 @@ export const TAGS = {
   ASTRO: {
     name: "Astro",
     icon: AstroIcon
+  },
+  ANGULAR: {
+    name: "Angular",
+    icon: AngularIcon
+  },
+  JAVA: {
+    name: "Java",
+    icon: JavaIcon
+  },
+  POSTGREST: {
+    name: "Postgrest",
+    icon: PostgrestIcon
+  },
+  PYTHON: {
+    name: "Python",
+    icon: PythonIcon
+  },
+  PHP: {
+    name: "Php",
+    icon: PhpIcon
+  },
+  LARAVEL: {
+    name: "Laravel",
+    icon: LaravelIcon
+  },
+  SQLSERVER: {
+    name: "Sql Server",
+    icon: SqlServerIcon
+  },
+  DOCKER: {
+    name: "Docker",
+    icon: DockerIcon
+  },
+  GIT: {
+    name: "Git",
+    icon: GitIcon
+  },
+  CSHARP: {
+    name: "C#",
+    icon: CSharpIcon
   }
 };
